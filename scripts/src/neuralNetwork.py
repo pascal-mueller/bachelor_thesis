884aba84aca1db00e4ee3a4bb0b6ad9444fa90fc
Pascal Müller <pamuelle@student.ethz.ch>
2022-01-19 22:45:29 +0100

    added NN from paper to test. added ELU back again.

--- scripts/src/neuralNetwork.py
+++ scripts/src/neuralNetwork.py
@@ -11,103 +11,130 @@ class NeuralNetwork(nn.Module):
         self.normalizeInput = nn.BatchNorm1d(1)
         
         # 1. Layer
-        self.conv1 = nn.Conv1d(1, 8, 65)
-        nn.init.xavier_uniform_(self.conv1.weight)
-        self.LeakyReLU1 = nn.LeakyReLU()
-        
+        self.conv1 = nn.Conv1d(1, 8, 64)
+        self.ELU1 = nn.ELU()
+        print(self.ELU1) 
         # 2. Layer
-        self.conv2 = nn.Conv1d(8, 8, 33)
-        nn.init.xavier_uniform_(self.conv2.weight)
+        self.conv2 = nn.Conv1d(8, 8, 32)
         self.maxpool1 = nn.MaxPool1d(4)
-        self.LeakyReLU2 = nn.LeakyReLU()
-        
+        self.ELU2 = nn.ELU()
+        print(self.ELU2)
         # 3. Layer
-        self.conv3 = nn.Conv1d(8, 16, 33)
-        nn.init.xavier_uniform_(self.conv3.weight)
-        self.LeakyReLU3 = nn.LeakyReLU()
+        self.conv3 = nn.Conv1d(8, 16, 32)
+        self.ELU3 = nn.ELU()
 
         # 4. Layer
-        self.conv4 = nn.Conv1d(16, 16, 17)
-        nn.init.xavier_uniform_(self.conv4.weight)
+        self.conv4 = nn.Conv1d(16, 16, 16)
         self.maxpool2 = nn.MaxPool1d(3)
-        self.LeakyReLU4 = nn.LeakyReLU()
+        self.ELU4 = nn.ELU()
         
         # 5. Layer
-        self.conv5 = nn.Conv1d(16, 32, 17)
-        nn.init.xavier_uniform_(self.conv5.weight)
-        self.LeakyReLU5 = nn.LeakyReLU()
+        self.conv5 = nn.Conv1d(16, 32, 16)
+        self.ELU5 = nn.ELU()
 
         # 6. Layer
-        self.conv6 = nn.Conv1d(32, 32, 17)
-        nn.init.xavier_uniform_(self.conv6.weight)
+        self.conv6 = nn.Conv1d(32, 32, 16)
         self.maxpool3 = nn.MaxPool1d(2)
-        self.LeakyReLU6 = nn.LeakyReLU()
+        self.ELU6 = nn.ELU()
         
         # 7. Layer
         self.flatten = nn.Flatten()
         
         # 8. Layer
-        self.linear1 = nn.Linear(1824, 64)
-        self.dropout1 = nn.Dropout(p=0.5) #TODO: Decide p
-        self.LeakyReLU7 = nn.LeakyReLU()
+        self.linear1 = nn.Linear(1856, 64)
+        self.dropout1 = nn.Dropout(p=0.8) #TODO: Decide p
+        self.ELU7 = nn.ELU()
         
         # 9. Layer
         self.linear2 = nn.Linear(64, 64)
-        self.dropout2 = nn.Dropout(p=0.5) #TODO: Decide p
-        self.LeakyReLU8 = nn.LeakyReLU()
+        self.dropout2 = nn.Dropout(p=0.8) #TODO: Decide p
+        self.ELU8 = nn.ELU()
 
         self.linear3 = nn.Linear(64, 2)
 
         self.output= nn.Softmax(dim=1)
 
     def forward(self, inputs):
+        print(self.ELU1)
+        print(self.ELU2)
         #z = self.network(inputs)
 
         # Input layer
         z = self.normalizeInput(inputs)
 
-        # 1. Layer - Conv1D + LeakyReLU
+        # 1. Layer - Conv1D + ELU
         z = self.conv1(z)
-        z = self.LeakyReLU1(z)
+        z = self.ELU1(z)
         
-        # 2. Layer - Conv1Da + MaxPool1D + LeakyReLU
+        # 2. Layer - Conv1Da + MaxPool1D + ELU
         z = self.conv2(z)
         z = self.maxpool1(z)
-        z = self.LeakyReLU2(z)
+        z = self.ELU2(z)
 
-        # 3. Layer - Conv1D + LeakyReLU
+        # 3. Layer - Conv1D + ELU
         z = self.conv3(z)
-        z = self.LeakyReLU3(z)
+        z = self.ELU3(z)
 
-        # 4. Layer - Conv1d + MaxPool1D + LeakyReLU
+        # 4. Layer - Conv1d + MaxPool1D + ELU
         z = self.conv4(z)
         z = self.maxpool2(z)
-        z = self.LeakyReLU4(z)
+        z = self.ELU4(z)
 
-        # 5. Layer - Conv1D + LeakyReLU
+        # 5. Layer - Conv1D + ELU
         z = self.conv5(z)
-        z = self.LeakyReLU5(z)
+        z = self.ELU5(z)
 
-        # 6. Layer - Conv1D + MaxPool1D + LeakyReLU
+        # 6. Layer - Conv1D + MaxPool1D + ELU
         z = self.conv6(z)
         z = self.maxpool3(z)
-        z = self.LeakyReLU6(z)
+        z = self.ELU6(z)
 
         # 7. Layer - Flatten
         z = self.flatten(z)
 
-        # 8. Layer - Linear + Dropout + LeakyReLU
+        # 8. Layer - Linear + Dropout + ELU
         z = self.linear1(z)
         z = self.dropout1(z)
-        z = self.LeakyReLU7(z)
+        z = self.ELU7(z)
 
-        # 9. Layer - Linear + Dropout + LeakyReLU
+        # 9. Layer - Linear + Dropout + ELU
         z = self.linear2(z)
         z = self.dropout2(z)
-        z = self.LeakyReLU8(z)
+        z = self.ELU8(z)
 
         # Output Layer - Linear + Softmax
         z = self.linear3(z)
         z = self.output(z)
 
         return z.unsqueeze(1)
+
+def get_network():
+    Network = nn.Sequential(
+        nn.BatchNorm1d(1),	# 1x2048
+        nn.Conv1d(1, 8, 64),	# 8x1985
+        nn.ELU(),
+        nn.Conv1d(8, 8, 32),	# 8x1954
+        nn.MaxPool1d(4),	# 8x488
+        nn.ELU(),
+        nn.Conv1d(8, 16, 32),	# 16x457
+        nn.ELU(),
+        nn.Conv1d(16, 16, 16),	# 16x442
+        nn.MaxPool1d(3),	# 16x147
+        nn.ELU(),
+        nn.Conv1d(16, 32, 16),	# 32x132
+        nn.ELU(),
+        nn.Conv1d(32, 32, 16),	# 32x117
+        nn.MaxPool1d(2),	# 32x58
+        nn.ELU(),
+        nn.Flatten(),	#  1856
+        nn.Linear(1856, 64),	# 64
+        nn.Dropout(p=.5),
+        nn.ELU(),
+        nn.Linear(64, 64),	# 64
+        nn.Dropout(p=.5),
+        nn.ELU(),
+        nn.Linear(64, 2),	# 2
+        nn.Softmax(dim=1)
+    )
+
+    return Network
