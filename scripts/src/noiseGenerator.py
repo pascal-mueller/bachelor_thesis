9e642b5381d5c40a4bebc0f006bcc55389c99951
Pascal Müller <pamuelle@student.ethz.ch>
2022-01-19 23:13:54 +0100

    changed PSD function call

--- scripts/src/noiseGenerator.py
+++ scripts/src/noiseGenerator.py
@@ -40,8 +40,9 @@ class NoiseGenerator:
         """
 
         # Generate noise from PSD
+        seed = int((time.time()* time.time() )% 200000 )
         noise_fn = pycbc.noise.gaussian.frequency_noise_from_psd
-        noise = noise_fn(self.psd).to_timeseries().numpy()
+        noise = noise_fn(self.psd, seed).to_timeseries().numpy()
         
         #idx = param['index']
         #self.dataset[idx] = noise
